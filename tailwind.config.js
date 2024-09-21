2f6d292681defd9bbaf098e0aedbb837b3e90a79
ishaanJ91 <jaini@tcd.ie>
2024-09-21 05:11:15 +0100

    landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+const { default: theme } = require("@material-tailwind/react/theme");
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      'primary':'#0d1117'
+    },
     extend: {},
   },
   plugins: [],
